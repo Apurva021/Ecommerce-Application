e291660beb9f9492e1e454629e708845dc69a58f
Apurva021 <apurvadegreat@gmail.com>
2021-04-08 12:37:48 +0530

    Added change password enpoint in gateway-service

--- gateway-service-v2/src/main/java/com/apurva/gatewayservicev1/GatewayController.java
+++ gateway-service-v2/src/main/java/com/apurva/gatewayservicev1/GatewayController.java
@@ -1,5 +1,7 @@
 package com.apurva.gatewayservicev1;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +36,14 @@ public class GatewayController {
 	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public boolean canAccess(HttpServletRequest request, Integer userIdInteger) {
+		String jwtString = request.getHeader("Authorization").substring(7);
+		String emailString = jwtUtil.extractUsername(jwtString);
+		Integer idInteger = Integer.parseInt(jwtUtil.getPayload(jwtString));
+		
+		return idInteger.equals(userIdInteger);
+	}
 	
 	@PostMapping("/signup")
 	public String register(@RequestBody User user) throws Exception{
@@ -72,5 +83,33 @@ public class GatewayController {
 		String jwtString = jwtUtil.generateToken(userDetails, Integer.toString(idInteger), user.getFirstNameString(), user.getLastNameString());
 		
 		return ResponseEntity.ok(new AuthenticationResponse(jwtString));
+	}
+	
+	@PostMapping("/change-password/{userIdInteger}")
+	public String changePassword(HttpServletRequest request, @RequestBody ChangePasswordRequest changePasswordRequest, @PathVariable Integer userIdInteger) throws Exception {
+		if(!canAccess(request, userIdInteger)) {
+			throw new Exception("Access denied to sensitive Resource!");
+		}
+		
+		User user = userRepository.findByUserIdInteger(userIdInteger);
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(user.getEmailString(), changePasswordRequest.getCurrentPasswordString()));
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			throw new Exception("Current password entered is wrong!");
+		}
+		
+		if(!changePasswordRequest.getNewPasswordString().equals(changePasswordRequest.getConfirmNewPasswordString())) {
+			throw new Exception("The new password fields do not match!");
+		}
+		
+		user.setPasswordString(bCryptPasswordEncoder.encode(changePasswordRequest.getNewPasswordString()));
+		userRepository.save(user);
+		
+		return "Password Updated Succesfully!";
+		
+		
 	}
 }
--- gateway-service-v2/src/main/java/com/apurva/gatewayservicev1/UserRepository.java
+++ gateway-service-v2/src/main/java/com/apurva/gatewayservicev1/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 	public User findByEmailString(String emailString);
+	public User findByUserIdInteger(Integer userIdInteger);
 }

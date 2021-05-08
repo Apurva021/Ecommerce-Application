16cb6b6243e9a43cc131d003cbd0081394fabbba
rvb2000 <66408706+rvb2000@users.noreply.github.com>
2021-05-08 17:42:20 +0530

    Added inventory service

---
+++ inventory-service/config.js
@@ -0,0 +1,17 @@
+module.exports={
+    server:{
+        "port":3004
+    },
+    db:{
+        'uri':"mongodb+srv://rutvik:17bit008@productcatalog.poqge.mongodb.net/ProductCatalog?retryWrites=true&w=majority",
+        'uriTest':"mongodb://localhost/bags-ecommerce"
+    },
+    kafka:{
+        "hostName":"localhost:9092",
+        "topics":{
+            "inventory":"InventoryStream",
+            "order":"OrderStream",
+            "payment":"paymentStream"
+        }
+    }
+}

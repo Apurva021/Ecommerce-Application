6640f6bbb1ea9ff804bf76b36102c2a0e63bc12a
Apurva021 <apurvadegreat@gmail.com>
2021-05-10 18:56:07 +0530

    Changes in product schema in recommender-service.

--- recommender-service/src/main/java/com/apurva/recommenderservice/Product.java
+++ recommender-service/src/main/java/com/apurva/recommenderservice/Product.java
@@ -11,20 +11,44 @@ public class Product {
 	private String sizeString;
 	private String[] imgs;
 	private Integer quantityBought;
-	private Boolean isAvailable;
+	private Boolean available;
 	private Manufacturer manufacturer;
+	private Double avgRating;
+	private Sizes sizes[];
+	private Category category;
 	
+	
+	
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public Sizes[] getSizes() {
+		return sizes;
+	}
+	public void setSizes(Sizes[] sizes) {
+		this.sizes = sizes;
+	}
+	public Boolean getAvailable() {
+		return available;
+	}
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+	public Double getAvgRating() {
+		return avgRating;
+	}
+	public void setAvgRating(Double avgRating) {
+		this.avgRating = avgRating;
+	}
 	public Manufacturer getManufacturer() {
 		return manufacturer;
 	}
 	public void setManufacturer(Manufacturer manufacturer) {
 		this.manufacturer = manufacturer;
-	}
-	public Boolean getIsAvailable() {
-		return isAvailable;
-	}
-	public void setIsAvailable(Boolean isAvailable) {
-		this.isAvailable = isAvailable;
 	}
 	public String getProductCode() {
 		return productCode;
@@ -85,6 +109,70 @@ public class Product {
 		public void setCompanyName(String companyName) {
 			this.companyName = companyName;
 		}
+		
+	}
+	
+	static class Sizes {
+		private String _id;
+		private String value;
+		private Integer stock;
+		public String get_id() {
+			return _id;
+		}
+		public void set_id(String _id) {
+			this._id = _id;
+		}
+		public String getValue() {
+			return value;
+		}
+		public void setValue(String value) {
+			this.value = value;
+		}
+		public Integer getStock() {
+			return stock;
+		}
+		public void setStock(Integer stock) {
+			this.stock = stock;
+		}
+	}
+	
+	static class Category{
+		private String[] subcategories;
+		private String _id;
+		private String title;
+		private String slug;
+		private Integer __v;
+		public String[] getSubcategories() {
+			return subcategories;
+		}
+		public void setSubcategories(String[] subcategories) {
+			this.subcategories = subcategories;
+		}
+		public String get_id() {
+			return _id;
+		}
+		public void set_id(String _id) {
+			this._id = _id;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getSlug() {
+			return slug;
+		}
+		public void setSlug(String slug) {
+			this.slug = slug;
+		}
+		public Integer get__v() {
+			return __v;
+		}
+		public void set__v(Integer __v) {
+			this.__v = __v;
+		}
+		
 		
 	}
 	
